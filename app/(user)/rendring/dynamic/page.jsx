6f379ad58a9799cbf572a47bfab81f854b8e0e23
Sachin <mrsachinsehrawat07@gmail.com>
2025-09-15 18:31:33 +0530

    use React Cache for boost performance

--- app/(user)/rendring/dynamic/page.jsx
+++ app/(user)/rendring/dynamic/page.jsx
@@ -1,18 +1,42 @@
 import { db } from "@/config/db";
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
+import { cache } from "react";
 
 const DynamicPage = async () => {
-  const [doctors] = await db.execute(`SELECT * FROM doctors`);
-  console.log("Dynamic Page");
-  
+  const doctors = await getAllDoctors()
+
+  return (
+    <>
+      <h1>Totol Doctors: {doctors.length}</h1>
+      <DoctorList doctors={doctors} />
+    </>
+  );
+};
+
+export default DynamicPage;
+
+const DoctorList = async () => {
+  const doctors = await getAllDoctors()
+  const doctors1 = await getAllDoctors()
+  const doctors2 = await getAllDoctors()
+  const doctors3 = await getAllDoctors()
+  const doctors4 = await getAllDoctors()
+  const doctors5 = await getAllDoctors()
+  const doctors6 = await getAllDoctors()
 
   return (
     <ul>
-      {doctors.map(doctor => (
-        <li key={doctor.doctor_id}>{doctor.doctor_id} {doctor.first_name} {doctor.last_name}</li>
+      {doctors?.map((doctor) => (
+        <li key={doctor.doctor_id}>
+          {doctor.doctor_id} {doctor.first_name} {doctor.last_name}
+        </li>
       ))}
     </ul>
   );
 };
 
-export default DynamicPage;
+const getAllDoctors = cache(async () => {
+  const [doctors] = await db.execute(`SELECT * FROM doctors`);
+  console.log("Fetching Doctors");
+  return doctors;
+});
